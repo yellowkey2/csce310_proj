d4c109a6301a439a43599bb50ce5220c63fb2207
Justin Heger <justin.heger@tamu>
2023-04-05 16:34:23 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>CSCE 310 Project</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <?php include('templates/header.php'); ?>
+        <?php include('templates/menu.php'); ?>
+
+        <h1>CSCE 310 Final Project</h1>
+        <p>My content</p>
+
+        <?php include('templates/footer.php'); ?>
+    </body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,4 @@
+<header>
+    <img src="images/logo.png" alt="logo" />
+    <h1>My Website</h1>
+</header>
