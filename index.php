c4668885033a39512f8beef9763ef55c69cbca66
justinheger <justin.heger@tamu.edu>
2023-04-17 20:10:36 -0500

    Added sample code to show data connection to server

--- index.php
+++ index.php
@@ -7,6 +7,33 @@
         <?php include('templates/header.php'); ?>
         <?php include('templates/menu.php'); ?>
 
+        <?php
+        $servername = "localhost";
+        $username = "justin";
+        $password = "password";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password);
+
+        // Check connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+        else
+        echo "Connected successfully <br>";
+        echo "<br>";
+        // Create new line
+
+        // Select users table information and display it on the website
+        $sql = "SELECT * FROM csce310_proj.users";
+        $result = $conn->query($sql);
+        while($row = $result->fetch_assoc()) {
+            #print each row (usr_id	usr_name	usr_passwd	board_id	profile_desc)
+            echo "id: " . $row["usr_id"]. "<br> Name: " . $row["usr_name"] .  "<br> Password: ". $row["usr_passwd"]. " <br>" . "<br>";
+        }
+        $conn->close();
+        ?>
+
         <h1>CSCE 310 Final Project</h1>
         <p>My content</p>
 
