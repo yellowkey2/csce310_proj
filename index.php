857a1ce021cadb1b7565b164b25eb8ae8c87c655
Sloan Davis <sloanbdavis@gmail.com>
2023-04-19 15:44:10 -0500

    move db connect to template

--- index.php
+++ index.php
@@ -1,48 +1,37 @@
 <html>
-    <head>
-        <title>CSCE 310 Project</title>
-        <link href="css/style.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-        <?php include('templates/header.php'); ?>
-        <?php include('templates/menu.php'); ?>
 
-        <?php
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
+<head>
+    <title>CSCE 310 Project</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
 
-        // Create connection
-        $conn = new mysqli($servername, $username, $password);
+<body>
+    <?php include('templates/header.php'); ?>
+    <?php include('templates/menu.php'); ?>
 
-        // Check connection
-        if ($conn->connect_error) {
+    <?php
+    include('templates/db_login.php');
+
+    // Create new line
+    // Select users table information and display it on the website
+    $sql = "SELECT * FROM csce310_db.users";
+    $result = $conn->query($sql);
+    while ($row = $result->fetch_assoc()) {
+        #print each row (usr_id	usr_name	usr_passwd	board_id	profile_desc)
+        echo "id: " . $row["usr_id"] . "<br> Name: " . $row["usr_name"] .  "<br> Password: " . $row["usr_passwd"] . " <br>" . "<br>";
+    }
+    $conn->close();
+    // Check that connection is closed
+    if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
-        }
-        else
-        echo "Connected successfully <br>";
-        echo "<br>";
-        // Create new line
-
-        // Select users table information and display it on the website
-        $sql = "SELECT * FROM csce310_db.users";
-        $result = $conn->query($sql);
-        while($row = $result->fetch_assoc()) {
-            #print each row (usr_id	usr_name	usr_passwd	board_id	profile_desc)
-            echo "id: " . $row["usr_id"]. "<br> Name: " . $row["usr_name"] .  "<br> Password: ". $row["usr_passwd"]. " <br>" . "<br>";
-        }
-        $conn->close();
-        // Check that connection is closed
-        if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-        }
-        else
+    } else
         echo "Connection closed successfully <br>";
-        ?>
+    ?>
 
-        <h1>CSCE 310 Final Project</h1>
-        <p>My content</p>
+    <h1>CSCE 310 Final Project</h1>
+    <p>My content</p>
 
-        <?php include('templates/footer.php'); ?>
-    </body>
+    <?php include('templates/footer.php'); ?>
+</body>
+
 </html>
