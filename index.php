1d45df87ee14f347fdc008394630ca0bca2de6ad
Sloan Davis <sloanbdavis@gmail.com>
2023-04-24 13:40:11 -0500

    add incorrect password message

--- index.php
+++ index.php
@@ -12,26 +12,35 @@
     include('templates/db_login.php');
     include('templates/menu.php');
     include('templates/header.php');
+
+    //add message if account already exists
     $alreadyCreatedAccount = "";
-    if (isset($_GET["accountExists"]) && $_GET["accountExists"] == true){
+    if (isset($_GET["accountExists"]) && $_GET["accountExists"] == true) {
         $alreadyCreatedAccount = "<p style='color:red;'>Cannot create account: Account already exists </p>";
+    }
+
+    //display message if password incorrect
+    $passwordIncorrectMessage = "";
+    if (isset($_GET["passwordIncorrect"]) && $_GET["passwordIncorrect"] == true) {
+        $passwordIncorrectMessage = "<p style='color:red;'>Incorrect Login Credentials </p>";
     }
     ?>
 
     <h1>Project Management System</h1>
     <p>Create Account</p>
     <form action="create_account.php" method="post">
-    <?php echo $alreadyCreatedAccount ?>
-    Username: <input type="text" name="usr_name"><br>
-    Password: <input type="text" name="usr_password"><br>
-    <input type="submit">
+        <?php echo $alreadyCreatedAccount ?>
+        Username: <input type="text" name="usr_name"><br>
+        Password: <input type="text" name="usr_password"><br>
+        <input type="submit">
     </form>
 
     <p>Login</p>
     <form action="login.php" method="get">
-    Username: <input type="text" name="usr_name"><br>
-    Password: <input type="text" name="usr_password"><br>
-    <input type="submit">
+        <?php echo $passwordIncorrectMessage ?>
+        Username: <input type="text" name="usr_name"><br>
+        Password: <input type="text" name="usr_password"><br>
+        <input type="submit">
     </form>
 
     <?php include('templates/footer.php'); ?>
--- login.php
+++ login.php
@@ -1,39 +1,23 @@
+<?php
+session_start();
+include("./templates/db_login.php");
 
-<!DOCTYPE html>
-<html>
- 
-<head>
-    <title>Login</title>
-</head>
- 
-<body>
-    <center>
-        <?php
-        include("./templates/db_login.php");
+$username = $_REQUEST['usr_name'];
+$password = $_REQUEST['usr_password'];
 
-        $username = $_REQUEST['usr_name'];
-        $password = $_REQUEST['usr_password'];
+$_SESSION['usr_name'] = $username;
 
-        session_start();
-        $_SESSION['usr_name'] = $username;
+// Check if username is already in the database
+$sql = "SELECT * FROM users WHERE usr_name = '" . $username . "' AND usr_passwd = '" . $password . "'";
+$result = $conn->query($sql);
 
-        // Check if username is already in the database
-        $sql = "SELECT * FROM users WHERE usr_name = '" . $username . "' AND usr_passwd = '" . $password . "'";
-        $result = $conn->query($sql);
+if ($result->num_rows == 0) {
+    // Redirect to login page
+    header("Location: index.php?passwordIncorrect=true");
+} else {
+    // Redirect to dashboard page
+    header("Location: dashboard.php");
+}
 
-        if ($result->num_rows == 0) {
-            echo "Username or password is incorrect";
-            // Redirect to login page
-            // header("Location: index.php");
-        } else {
-            echo "Login successful";
-            // Redirect to dashboard page
-            header("Location: dashboard.php");
-        }
-
-        mysqli_close($conn);
-        ?>
-    </center>
-</body>
-
-</html>
+mysqli_close($conn);
+?>
