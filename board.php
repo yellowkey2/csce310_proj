a6fd61397fbdf08f7793c80caa2a273a5242f075
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-04-26 11:20:32 -0500

    create templates for board

--- board.php
+++ board.php
@@ -8,19 +8,19 @@ include("templates/db_login.php");
 <body>
     <?php
     include('templates/header.php');
-    
+
     //make sure we have a board id
     $cur_board_id = "";
-    if(!isset($_GET["boardID"])){
+    if (!isset($_GET["boardID"])) {
         echo "no board found...";
         exit;
     }
     $cur_board_id = $_GET["boardID"];
-    
+
     //make sure user has authorization to view this board 
     $sql = "SELECT * FROM board_assignments WHERE usr_id = " . $_SESSION['usr_id'] . " AND board_id = " . $cur_board_id;
     $result = $conn->query($sql);
-    if($result->num_rows == 0){
+    if ($result->num_rows == 0) {
         echo "Incorrect Board Access";
         exit;
     }
@@ -28,23 +28,28 @@ include("templates/db_login.php");
     $access_level = $row['access_level'];
     $_SESSION["access_level"] = $access_level;
     //send user to correct board page based on access level
-    if($access_level == 0){
-        header("Location: adminBoard.php");
-    }
-    else if($access_level == 1){
-        header("Location: editorBoard.php");
-    }
-    else if($access_level == 2){
-        header("Location viewerBoard.php");
-    }
-    else{
-        echo "no valid access level";
-        exit;
-    }
-    
+    // if ($access_level == 0) {
+    //     header("Location: adminBoard.php");
+    // } else if ($access_level == 1) {
+    //     header("Location: editorBoard.php");
+    // } else if ($access_level == 2) {
+    //     header("Location viewerBoard.php");
+    // } else {
+    //     echo "no valid access level";
+    //     exit;
+    // }
+
 
     $conn->close();
     ?>
+    <div id="boardContainer">
+        <div id="boardItems"></div>
+        <div id="boardAppointments"></div>
+        <div id="boardComments"></div>
+    </div>
+
+
+
 </body>
 
 
