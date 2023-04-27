68f9f4dabc93705713e06b739f1bd5a9027775f9
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-04-26 19:02:20 -0500

    comment new board templates

--- boardTemplates/appointmentsEditor.php
+++ boardTemplates/appointmentsEditor.php
@@ -1,1 +1,6 @@
+<!-- This is a component that will be rendered in the Dashboard to display board appointments with editing capability -->
 
+<?php
+session_start();
+include("templates/db_login.php");
+?>
--- boardTemplates/itemsViewer.php
+++ boardTemplates/itemsViewer.php
@@ -1,3 +1,5 @@
+<!-- This is a component that will be rendered in the Dashboard to display board items without editing capability -->
+
 <?php
 session_start();
 include("templates/db_login.php");
