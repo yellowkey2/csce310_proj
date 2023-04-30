d0c2a53a3c6b19c67d74ecb383f3c75bef9da1b9
313-logtempacct <losers.arent.winners@gmail.com>
2023-04-30 00:59:30 -0500

    revert back to login page a couple seconds after a new account is created

--- create_account.php
+++ create_account.php
@@ -1,11 +1,11 @@
 <!-- This file takes care of creating an account for a new user -->
 <!-- This file was created by Justin Heger -->
+<!-- This file was edited by Logan Talton -->
 
 <?php
 session_start();
 include('./templates/db_login.php');
 
-$_SESSION['usr_name'] = $username;
 $username = $_REQUEST['usr_name'];
 $password = $_REQUEST['usr_password'];
 
@@ -15,14 +15,18 @@ $result = $conn->query($sql);
 if($result->num_rows > 0){
     $account_exists = true;
     header("Location: index.php?accountExists=$account_exists");
+    exit();
 }
-
-
 
 $sql = "INSERT INTO users (usr_name, usr_passwd) VALUES ('" . $username . "', '" . $password . "')";
 
 if (mysqli_query($conn, $sql)) {
     echo "Records added successfully.";
+    echo '<script type="text/javascript">
+              window.setTimeout(function(){
+                  window.history.back();
+              }, 2000);
+          </script>';
 } else {
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
 }
