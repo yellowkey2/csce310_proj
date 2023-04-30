d5975fcd5444d414672ac47d6f99bdbeb9b77cfc
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-04-30 14:55:05 -0500

    fix sql errors

--- csce310_db.sql
+++ csce310_db.sql
@@ -121,37 +121,37 @@ INSERT INTO `users` (`usr_id`, `usr_name`, `usr_passwd`, `board_id`, `profile_de
 -- Indexes for table `appointment`
 --
 ALTER TABLE `appointment`
-  ADD PRIMARY KEY (`appt_id`),
+  ADD PRIMARY KEY (`appt_id`)
 
 --
 -- Indexes for table `appointment_assignments`
 --
 ALTER TABLE `appointment_assignments`
-  ADD PRIMARY KEY (`app_assignment_id`),
+  ADD PRIMARY KEY (`app_assignment_id`)
 
 --
 -- Indexes for table `board`
 --
 ALTER TABLE `board`
-  ADD PRIMARY KEY (`board_id`),
+  ADD PRIMARY KEY (`board_id`)
 
 --
 -- Indexes for table `board_assignments`
 --
 ALTER TABLE `board_assignments`
-  ADD PRIMARY KEY (`assignment_id`),
+  ADD PRIMARY KEY (`assignment_id`)
 
 --
 -- Indexes for table `board_item`
 --
 ALTER TABLE `board_item`
-  ADD PRIMARY KEY (`item_id`),
+  ADD PRIMARY KEY (`item_id`)
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`usr_id`),
+  ADD PRIMARY KEY (`usr_id`)
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -191,7 +191,7 @@ ALTER TABLE `board_item`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `usr_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `usr_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123456;
 
 --
 -- Constraints for dumped tables
