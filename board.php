f6eb0b31406cbf976bb215b58ffadb27ad17068e
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-05-01 22:34:00 -0500

    remove deprecated code

--- board.php
+++ board.php
@@ -46,35 +46,6 @@ include("templates/db_login.php");
     //     echo "no valid access level";
     //     exit;
     // }
-
-    // adding a user to the board handling
-    if (isset($_POST['addUser'])) {
-        $newUser = $_POST['newUser'];
-        // Check if user ID exists
-        $sql = "SELECT * FROM users WHERE usr_id = " . $newUser;
-        $result = $conn->query($sql);
-        if ($result->num_rows == 0) {
-            echo "User ID not found.";
-        } else {
-            // Check if user is already assigned to board
-            $sql = "SELECT * FROM board_assignments WHERE board_id = $cur_board_id AND usr_id = $newUser";
-            $result = $conn->query($sql);
-            if ($result->num_rows > 0) {
-                echo "User is already assigned to this board.";
-            } else {
-                // Add new row to board_assignments table
-                $sql = "INSERT INTO board_assignments (board_id, usr_id, access_level) VALUES ($cur_board_id, $newUser, 2)";
-                if ($conn->query($sql) === TRUE) {
-                    echo "User added to board.";
-                    // Reload current page
-                    header("Refresh:0");
-                } else {
-                    echo "Error adding user: " . $conn->error;
-                }
-            }
-        }
-    }
-
     $conn->close();
     ?>
     <div id="boardContainer">
@@ -96,8 +67,8 @@ include("templates/db_login.php");
         </div>
     </div>
     <hr>
-    <?php 
-    if($access_level == 0){
+    <?php
+    if ($access_level == 0) {
         include("add_board_users.php");
     }
     ?>
