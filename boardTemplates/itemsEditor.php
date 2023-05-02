9653b918226df923d2b4280aa37fe3e96722331d
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-05-02 16:22:36 -0500

    add item to board

--- boardTemplates/itemsEditor.php
+++ boardTemplates/itemsEditor.php
@@ -15,7 +15,20 @@ if ($_SESSION['b_id_' . $board_id] == false) {
     exit;
 }
 
-//handle queries
+//handle query for add item
+if(isset($_POST['addItemBtn']) && isset($_POST['itemContent'])){
+    $usr_id = $_SESSION['usr_id'];
+    $timestamp = date('Y-m-d', time());
+    $post_type = 0;
+    $content = $_POST['itemContent'];
+
+    $sql = "INSERT INTO board_item (item_timestamp, item_posttype, item_content, usr_id, board_id) 
+    VALUES ('$timestamp', '$post_type', '$content', $usr_id, $board_id)";
+    $conn->query($sql);
+
+}
+
+//handle queries for update and delete
 $item_id = "";
 if (isset($_POST['item'])) {
     $item_id = $_POST['item'];
@@ -40,6 +53,10 @@ if (isset($_POST['update'])) {
 $sql = "SELECT * FROM board_item WHERE board_id = '" . $board_id . "'";
 $items = $conn->query($sql);
 while ($row = $items->fetch_assoc()) {
+    //skip if comment
+    if($row['item_posttype'] > 1){
+        continue;
+    }
     $checked = "";
     if ($row['item_posttype'] == '1') {
         $checked = "checked";
@@ -55,3 +72,10 @@ while ($row = $items->fetch_assoc()) {
 }
 
 ?>
+<div id="addBoardItem">
+    <h3>Add Item:</h3>
+     <form method="POST">
+        <input type="text" name="itemContent" placeholder='Item Text'>
+        <button type="submit" name="addItemBtn">Submit</button>
+     </form>
+</div>
